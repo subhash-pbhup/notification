488574c842f4d2fc6d5e90a59c599b8cc4f857b0
subhashbhai <subhashwebkul@gmail.com>
2024-10-21 13:34:53 +0530

    added code

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging.js');
+
+const firebaseConfig = {
+          apiKey: "AIzaSyAETpT7RzfSU9nhRTsX3dQDxTUOb-1cNV4",
+          authDomain: "test-f8ca0.firebaseapp.com",
+          projectId: "test-f8ca0",
+          storageBucket: "test-f8ca0.appspot.com",
+          messagingSenderId: "244259946058",
+          appId: "1:244259946058:web:5748847d9a796463da70b3",
+          measurementId: "G-BGR4SND4RN"
+        };
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
