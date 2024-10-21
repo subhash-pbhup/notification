36a391dc33fbe4c57ce85e207e71042f193e89b0
subhashbhai <subhashwebkul@gmail.com>
2024-10-21 17:32:01 +0530

    added code

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -14,15 +14,3 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
-
-
-messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message: ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.icon
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
